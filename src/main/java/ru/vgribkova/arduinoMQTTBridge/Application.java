63c55a64ad750b7cc126c793be938779c29f4075
Victoria Gribkova <viki87@bk.ru>
2020-11-29 20:18:03 +0300

    finished program

--- src/main/java/ru/vgribkova/arduinoMQTTBridge/Application.java
+++ src/main/java/ru/vgribkova/arduinoMQTTBridge/Application.java
@@ -1,6 +1,14 @@
 package ru.vgribkova.arduinoMQTTBridge;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 
 public class Application {
     private static final Logger logger = LogManager.getLogger();
@@ -9,8 +17,23 @@ public class Application {
         Arduino arduino = new Arduino("COM3", 9600);
         boolean connected = arduino.openConnection();
         System.out.println("Соединение установлено: " + connected);
-        arduino.serialRead("addresses.",message ->{
+        arduino.serialRead("DONE",message ->{
             logger.debug(message);
+            try{
+                JSONObject messageObject = new JSONObject(message);
+                JSONArray thermometers = messageObject.getJSONArray("thermometers");
+                List<String> ids = new ArrayList<>();
+                for (Object o : thermometers) {
+                    JSONObject thermometer = (JSONObject) o;
+                    String thermometerId = thermometer.getString("address");
+                    ids.add(thermometerId);
+                    float temperature = thermometer.getFloat("temperatureC");
+                    client.sendMessageTo("/heat/temperature/" + thermometerId, Float.toString(temperature));
+                }
+                client.sendMessageTo("/heat/temperature/ids", String.join(" ", ids));
+            }catch (JSONException e){
+                logger.error("Message not parsed: " + message);
+            }
             client.sendMessageTo("/heat/temperature/", message);
         });
 
